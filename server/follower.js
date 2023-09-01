47ebcd6536acaca243e0c5b5758c6d42c1169061
ofeky8 <118596031+ofeky8@users.noreply.github.com>
2023-09-01 15:14:46 +0300

    added tests and packaegejson

--- server/follower.js
+++ server/follower.js
@@ -17,19 +17,32 @@ const knex = require('./knex'); // Import the knex connection from the knex.js f
 // Follow user 
 async function follow(userID, followBy) {
   try {
+    // Check if userID exists in the users table
+    const userToBeFollowedExists = await knex('users').where({ userID }).first();
+    if (!userToBeFollowedExists) {
+      throw new Error(`User with ID: ${userID} does not exist.`);
+    }
+
+    // Check if followBy user exists in the users table
+    const followingUserExists = await knex('users').where({ userID: followBy }).first();
+    if (!followingUserExists) {
+      throw new Error(`User with ID: ${followBy} does not exist.`);
+    }
+
     const [followerId] = await knex('follower').insert({
       userID: userID,
       followBy: followBy,
     });
 
     console.log('Follower data saved with ID:', followerId);
-    console.log(`user id:${userID} is followd by ${followBy}`)
+    console.log(`user id:${userID} is followed by ${followBy}`)
     return followerId;
   } catch (error) {
     console.error('Error following user:', error);
     throw error;
   }
 }
+
 
 // Unfollow user 
 async function unFollow(userID, followBy) {
@@ -57,19 +70,27 @@ async function unFollow(userID, followBy) {
 // Get followers 
 async function getFollowers(userID) {
   try {
-    const followers  = await knex('follower')
+    // Check if userID exists in the users table
+    const userExists = await knex('users').where({ userID }).first();
+
+    if (!userExists) {
+      throw new Error(`User with ID: ${userID} does not exist.`);
+    }
+
+    const followers = await knex('follower')
       .select('userID')
       .where({
         followBy: userID,
-      }); 
+      });
 
     console.log('get followers for user', userID);
-    return followers; // Return the number of deleted rows
+    return followers;
   } catch (error) {
     console.error('Error getting followers:', error);
     throw error;
   }
 }
+
 
 const getAllUsers = async () => {
   return knex('users').select('userID', 'username');
